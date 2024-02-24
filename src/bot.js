12285d0f238b8febfcc3570cece831d409529939
Cordtus <Cordtus@gmail.com>
2024-02-23 22:31:19 -0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/bot.js

--- src/bot.js
+++ src/bot.js
@@ -10,19 +10,18 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
   const session = getSession(ctx.chat.id);
   session.lastAction = 'start';
-  ctx.reply(
-    'Welcome! Please send me an image or a static sticker.',
-  );
+  ctx.reply('Welcome! Please send me an image or a static sticker.');
+});
 
 bot.on('photo', processImageMessage);
 bot.on('document', processImageFileMessage);
 bot.on('sticker', ctx => {
-    if (ctx.message.sticker.is_animated) {
-        ctx.reply('Currently, animated stickers are not supported. Please send a static image or sticker.');
-    } else {
-        processImageMessage(ctx);
-    }
-});
+  if (ctx.message.sticker.is_animated) {
+    ctx.reply('Currently, animated stickers are not supported. Please send a static image or sticker.');
+  } else {
+    processImageMessage(ctx);
+  }
+}); // Ensure there's only one closing brace and one closing parenthesis here.
 
 bot.launch();
 console.log('Bot is running');
